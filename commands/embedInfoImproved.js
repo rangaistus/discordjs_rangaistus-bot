d4e665e4780b4d3a2d33f1796855f2cb970d16a4
rangaistus <6002029@mborijnland.nl>
2023-04-16 18:20:51 +0200

    Added a checker if the desired user is not a bot, and if the user is not a bot, it will not send the embed.

--- commands/embedInfoImproved.js
+++ commands/embedInfoImproved.js
@@ -4,13 +4,15 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('embed-info')
         .setDescription('Displays user information with a embed.')
-        .addUserOption(option => 
+        .addUserOption(option =>
             option.setName('user')
                 .setDescription('Select a user.')
                 .setRequired(true)
         ),
     async execute(interaction) {
         const user = interaction.options.getUser('user');
+        const bot = interaction.client.user;
+
         if (!user) {
             return await interaction.reply('User not found.');
         }
@@ -20,32 +22,40 @@ module.exports = {
             return await interaction.reply('User not found in the server.');
         }
 
+        if (bot) {
+            if (user.id === bot.id) {
+                return await interaction.reply('I am not a user.');
+            }
+        }
+
         const createdDate = user.createdAt ? user.createdAt.toLocaleString() : 'Unable to get user information.';
         const joinedDate = guildMember.joinedAt ? guildMember.joinedAt.toLocaleString() : 'Not available (user has not joined the server)';
 
-        await interaction.reply({ embeds: [{
-            color: '3447003',
-            author: {
-                name: user.tag,
-                icon_url: user.avatarURL()
-            },
-            thumbnail: {
-                url: user.avatarURL()
-            },
-            fields: [
-                {
-                    name: 'ID',
-                    value: user.id,
+        await interaction.reply({
+            embeds: [{
+                color: '3447003',
+                author: {
+                    name: user.tag,
+                    icon_url: user.avatarURL()
                 },
-                {
-                    name: 'Account Created At',
-                    value: createdDate,
+                thumbnail: {
+                    url: user.avatarURL()
                 },
-                {
-                    name: 'Joined Server At',
-                    value: joinedDate,
-                }
-            ]
-        }] });
+                fields: [
+                    {
+                        name: 'ID',
+                        value: user.id,
+                    },
+                    {
+                        name: 'Account Created At',
+                        value: createdDate,
+                    },
+                    {
+                        name: 'Joined Server At',
+                        value: joinedDate,
+                    }
+                ]
+            }]
+        });
     },
 };
