d92c94a4d457f770f9ca308a93d53d7d3ed1a385
rangaistus <6002029@mborijnland.nl>
2023-05-14 21:34:06 +0200

    loveCalculator.js

---
+++ commands/loveCalculator.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('love-meter')
+        .setDescription('Measures the love between two users')
+        .addUserOption(option =>
+            option.setName('user1')
+                .setDescription('Select the first user.')
+                .setRequired(true)
+        )
+        .addUserOption(option =>
+            option.setName('user2')
+                .setDescription('Select the second user.')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const user1 = interaction.options.getUser('user1');
+        const user2 = interaction.options.getUser('user2');
+
+        const love = Math.floor(Math.random() * 101);
+
+        const loveBar = '█'.repeat(Math.floor(love / 10));
+        const emptyBar = '░'.repeat(10 - Math.floor(love / 10));
+
+        await interaction.reply(`:sparkling_heart:  ${user1.toString()} and ${user2.toString()}'s love meter: ${love}%\n\n[${loveBar}${emptyBar}]`);
+    },
+};
