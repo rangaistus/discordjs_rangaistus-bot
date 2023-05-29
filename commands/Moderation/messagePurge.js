799ecbaa50f5ead36002b0fb643ed366e142e2c9
rangaistus <6002029@mborijnland.nl>
2023-05-29 21:46:41 +0200

    Added Purge command to Moderation that deletes a specified number of messages from a channel.

---
+++ commands/Moderation/messagePurge.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Bulk deletes a specified number of messages.')
+        .addStringOption(option =>
+            option.setName('amount')
+                .setDescription('How much you want to delete')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const randomColor = Math.floor(Math.random() * 16777215);
+        const count = interaction.options.getString('amount');
+
+        await interaction.deferReply({ ephemeral: true });
+        if (count > 200 || count < 1) return interaction.editReply({
+            embeds: [{
+                color: randomColor,
+                description: 'You can only delete between 1 and 200 messages.',
+            }]
+        });
+        await interaction.channel.bulkDelete(count);
+        await interaction.editReply({
+            embeds: [{
+                color: randomColor,
+                description: `Deleted ${count} messages.`,
+            }]
+        });
+
+    }
+}
